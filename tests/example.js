4268f339056638d6aecd7363bd289bf508e35970
stephenb <stephen.bartholomew@paxport.net>
2019-09-17 15:29:30 +0100

    More tests2

--- tests/example.js
+++ tests/example.js
@@ -10,6 +10,7 @@ const pressKey = require('../lib/helpers').pressKey
 const shouldExist = require('../lib/helpers').shouldExist
 const shouldNotExist = require('../lib/helpers').shouldNotExist
 const utils = require('../lib/utils')
+const LOGIN_FORM = '#login_form'
 
 describe('My first puppeteer test', () => {
 	let browser
@@ -44,13 +45,34 @@ describe('My first puppeteer test', () => {
 
 		it('should click on sign in button', async() => {
 			await click(page, '#signin_button')
-			await shouldExist(page, '#login_form')
+			await shouldExist(page, LOGIN_FORM)
 		})
 		it('should submit login form', async() => {
 			await typeText(page, utils.generateID(), '#user_login')
 			await typeText(page, utils.generateNumbers(), '#user_password')
 			await click(page, '.btn-primary')
-			await shouldExist(page, '#login_form')
+			await shouldExist(page, LOGIN_FORM)
+		})
+		it('should	get error message', async() => {
+			await waitForText(page, 'body', 'Login and/or password are wrong')
+			await shouldExist(page, LOGIN_FORM)
+		})
+	})
+
+	describe('Search Test', () =>{
+		it('should navigate to the homepage', async () =>{
+			await loadUrl(page, config.baseURL)
+			await shouldExist(page, '#online_banking_features')
+		})
+		
+		it('should submit search phrase', async () =>{
+			await typeText(page, "hello world", '#searchTerm')
+			await pressKey(page, 'Enter')
+		})
+		it('shoulddisplay search results', async () =>{
+			await waitForText(page, 'h2', "Search Results")
+			await waitForText(page, 'body', "No results were found for the query")
+			
 		})
 	})
 
